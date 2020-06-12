6bbb47ebb86270b6dd80fb94d4d74fd3e3bee611
sarika296 <sarikadewangan94@gmail.com>
2020-06-12 22:47:52 +0530

    using ternary operator for testing of buy cake

--- src/redux/action/actions.test.js
+++ src/redux/action/actions.test.js
@@ -3,14 +3,15 @@ import Action from './actionTypes'
 
 describe("Actions Testing", () => {
     it(`should create an action as ${Action.BUY_CAKE}`, () => {
-        const quantity = 1
-        const cost = 500
-        const payload = 1
+        const quantity = 11
+        const qnt = quantity > 10 ? 0 : quantity 
+        const payload = qnt
+        const cost = qnt * 500
         const expectedAction = {
             type: Action.BUY_CAKE,
             payload,
             cost
         }
-        expect(actions.buyCake(quantity)).toEqual(expectedAction)
+        expect(actions.buyCake(qnt)).toEqual(expectedAction)
     });
 })
