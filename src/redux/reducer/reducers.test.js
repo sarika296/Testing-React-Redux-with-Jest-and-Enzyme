9e136e7e4f717ca70f5ba8819bdf25aeaa1caba9
sarika296 <sarikadewangan94@gmail.com>
2020-06-13 12:52:18 +0530

    testing of chocolate reducer completed

--- src/redux/reducer/reducers.test.js
+++ src/redux/reducer/reducers.test.js
@@ -11,6 +11,11 @@ const iceCreamInitialState = {
     totalMoney: 0
 }
 
+const chocolateInitialState = {
+    numOfChocolates: 30,
+    totalMoney: 0
+}
+
 describe("Reducers Testing", () => {
     
     it('should create a reducer of BUY_CAKE', () => {
@@ -28,6 +33,15 @@ describe("Reducers Testing", () => {
         const cost = 200 * payload
         const expectedState = { numOfIceCreams: 10, totalMoney: cost }
         const newState = Reducer(iceCreamInitialState,  { type: Action.BUY_ICECREAM, payload, cost } )
+        expect(newState).toEqual(expectedState)
+    });
+
+    it('should create a reducer of BUY_CHOCOLATE', () => {
+
+        const payload = 10
+        const cost = 100 * payload
+        const expectedState = { numOfChocolates: 20, totalMoney: cost }
+        const newState = Reducer(chocolateInitialState,  { type: Action.BUY_CHOCOLATE, payload, cost } )
         expect(newState).toEqual(expectedState)
     });
 
